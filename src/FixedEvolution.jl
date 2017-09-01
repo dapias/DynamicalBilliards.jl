5f856991608b9282c77555feeeb35239e68254d9
Diego Tapias <diego@maths.usyd.edu.au>
2017-09-01 20:51:07 +1000

    Create displacement function

--- src/FixedEvolution.jl
+++ src/FixedEvolution.jl
@@ -1,4 +1,4 @@
-export fixed_evolution!
+export fixed_evolution!, displacement!
 """
 Evolve the particle exactly until time t (slight modification of
 evolve!)
@@ -59,8 +59,65 @@ function fixed_evolution!(p::Particle, bt::Vector{Obstacle}, t)
     push!(rpos, p.pos + p.current_cell)
     push!(rvel, p.vel)
     push!(rt, t_to_write + tmin)
+
+    rt = cumsum(rt)
     
 
     return (rt, rpos, rvel)
+end
+
+"""
+Returns the initial and the final position in an array
+"""
+function displacement!(p::Particle, bt::Vector{Obstacle}, t)
+    if t <= 0
+        error("`evolve!()` cannot evolve backwards in time.")
+    end
+    rt = Float64[]
+    rpos = SVector{2,Float64}[]
+    push!(rpos, p.pos)
+
+    count = zero(t)
+    colobst_idx = 1
+    t_to_write = 0.0
+
+    while count < t
+        # Declare these because `bt` is of un-stable type!
+        tcol::Float64 = 0.0
+        tmin::Float64 = Inf
+
+        for i in eachindex(bt)
+            tcol = collisiontime(p, bt[i])
+            # Set minimum time:
+            if tcol < tmin
+                tmin = tcol
+                colobst_idx = i
+            end
+        end#obstacle loop
+
+        # set counter
+        count += increment_counter(t, tmin)
+        if count > t
+            count -= tmin
+            break
+        end
+
+        propagate!(p, tmin)
+        resolvecollision!(p, bt[colobst_idx])
+
+        t_to_write += tmin
+
+        if typeof(bt[colobst_idx]) <: PeriodicWall
+            continue
+        else
+            t_to_write = 0.0
+        end
+    end#time loop
+
+    tmin = t - count 
+    propagate!(p, tmin)
+    push!(rpos, p.pos + p.current_cell)
+
+    return rpos
 end
 
