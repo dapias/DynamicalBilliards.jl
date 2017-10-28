d134fc5c5f600de2bc3b331b264550ca12df9623
Diego Tapias <dandroveros@gmail.com>
2017-10-28 21:02:38 +1100

    Add observable xdistance

--- src/Observables.jl
+++ src/Observables.jl
@@ -1,4 +1,4 @@
-export lyapunovmaximum, distance
+export lyapunovmaximum, distance, xdistance
 
 import Base.copy 
 function copy(p::Particle{T}) where {T<:AbstractFloat}
@@ -37,4 +37,29 @@ function distance(particle::Particle{T}, bt::Vector{<:Obstacle{T}}, t::T) where 
     propagate!(p, tmin)
     push!(rpos, p.pos + p.current_cell)
     d = norm(rpos[2]-rpos[1])
+end
+
+function xdistance(particle::Particle{T}, bt::Vector{<:Obstacle{T}}, t::T) where {T<:AbstractFloat}
+    p = copy(particle)
+    rpos = SVector{2,T}[]
+    push!(rpos, p.pos)
+    count = zero(T)
+
+    while count < t
+        tmin::T, i::Int = next_collision(p, bt)
+        # set counter
+        if count +  increment_counter(t, tmin) > t
+            break
+        end
+        #####
+        tmin = relocate!(p, bt[i], tmin)
+        resolvecollision!(p, bt[i])
+        count += DynamicalBilliards.increment_counter(t, tmin)
+    end#time loop
+
+    tmin = t - count 
+    propagate!(p, tmin)
+    push!(rpos, p.pos + p.current_cell)
+    #    d = norm(rpos[2]-rpos[1])
+    d = rpos[2][1] - rpos[1][1] #distance in x
 end
